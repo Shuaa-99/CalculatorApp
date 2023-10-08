bfda405b7a5a6ac624b9e4242c1332e412b4ab25
Shua'a <senshen2018@gmail.com>
2023-10-08 17:38:53 +0300

    Initial Commit

---
+++ Calculator App/Calculator_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Calculator_AppApp.swift
+//  Calculator App
+//
+//  Created by SHUAA on 09/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct Calculator_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
